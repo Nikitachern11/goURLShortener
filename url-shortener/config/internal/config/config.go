9bfd1e367d05be7ff00a766c11beb8e31e3855e7
Nikita <nikita@example.com>
2025-02-07 23:52:32 +0300

    local.yaml done, config.go started

---
+++ url-shortener/config/internal/config/config.go
@@ -0,0 +1,31 @@
+package config
+
+import "time"
+import "os"
+import "log"
+
+type Config struct {
+	Env         string `yaml:"env" env-required:"true"`
+	StoragePath string `yaml:"storage_path" env-required:"true"`
+	HTTPServer  `yaml:"http_server"`
+}
+
+type HTTPServer struct {
+	Address     string        `yaml:"address" env-default:"localhost:8080"`
+	Timeout     time.Duration `yaml:"timeout" env-default:"4s"`
+	IdleTimeout time.Duration `yaml:"idle_timeout" env-default:"60s"`
+}
+
+func MustLoad() {
+	configPath := os.Getenv("CONFIG_PATH")
+	if configPath == "" {
+		log.Fatal("CONFIG_PATH is not set")
+	}
+
+	// check if file exists
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatalf("config file does not exist: %s", configPath)
+	}
+
+	var cfg Config
+}
