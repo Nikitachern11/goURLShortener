0dbb096afc17675a8b56e5d8d55c2620f77cd6d6
Nikita <nikita@example.com>
2025-02-16 00:47:16 +0300

    logger added

--- url-shortener/cmd/url-shortener/main.go
+++ url-shortener/cmd/url-shortener/main.go
@@ -1,15 +1,25 @@
 package main
 
 import (
-	"fmt"
 	"goUrlShortener/url-shortener/internal/config"
+	"log/slog"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
 
 	cfg := config.MustLoad()
 
-	fmt.Println(cfg)
+	log := setupLogger(cfg.Env)
+
+	log.Info("starting and checking logger", slog.String("env", cfg.Env))
+	log.Debug("debug message from logger")
 
 	// TODO: init logger: slog
 
@@ -18,5 +28,23 @@ func main() {
 	// TODO: init router: chi, "chi render"
 
 	//TODO: run server
+
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+
+	case envDev:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+
+	case envProd:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+
+	}
+
+	return log
 
 }
